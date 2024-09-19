9cd79eff2de463ba9ffd30d701de41b250e1d00a
Rangga Biner <ranggabiner@gmail.com>
2024-09-19 14:50:59 +0700

    Initial Commit

---
+++ Breesix/BreesixApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreesixApp.swift
+//  Breesix
+//
+//  Created by Rangga Biner on 19/09/24.
+//
+
+import SwiftUI
+
+@main
+struct BreesixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Breesix/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Breesix
+//
+//  Created by Rangga Biner on 19/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
