a288fbd949797ec99491275da25284aa9448447b
Rangga Biner <ranggabiner@gmail.com>
2024-09-29 22:50:51 +0700

    fix app file

--- Breesix/BreesixApp.swift
+++ Breesix/BreesixApp.swift
@@ -6,12 +6,30 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct BreesixApp: App {
+    let container: ModelContainer
+    
+    init() {
+        do {
+            container = try ModelContainer(for: Student.self)
+        } catch {
+            fatalError("Failed to create ModelContainer for  Note: \(error)")
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            let context = container.mainContext
+            let dataSource = StudentDataSourceImpl(context: context)
+            let repository = StudentRepositoryImpl(dataSource: dataSource)
+            let useCases = StudentUseCase(repository: repository)
+            let viewModel = StudentListViewModel(useCases: useCases)
+            
+            StudentListView(viewModel: viewModel)
         }
+        .modelContainer(container)
     }
 }
