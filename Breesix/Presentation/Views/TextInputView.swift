0cd305c86f83c33d117ff3632bacf81a79fc5cb2
Rangga Biner <ranggabiner@gmail.com>
2024-10-15 13:55:47 +0700

    all task done

---
+++ Breesix/Presentation/Views/TextInputView.swift
@@ -0,0 +1,18 @@
+//
+//  TextInputView.swift
+//  Breesix
+//
+//  Created by Rangga Biner on 15/10/24.
+//
+
+import SwiftUI
+
+struct TextInputView: View {
+    var body: some View {
+        Text("Teks Input ini")
+    }
+}
+
+#Preview {
+    TextInputView()
+}
