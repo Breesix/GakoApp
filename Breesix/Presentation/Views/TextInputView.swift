a9b909f86c50747343f50a26205e71433b69f6a0
Rangga Biner <ranggabiner@gmail.com>
2024-10-16 06:32:12 +0700

    pro tips visible hide if keyboard appear

--- Breesix/Presentation/Views/TextInputView.swift
+++ Breesix/Presentation/Views/TextInputView.swift
@@ -11,6 +11,8 @@ struct TextInputView: View {
     @Environment(\.presentationMode) var presentationMode
     @State private var textInput: String = ""
     @State private var showAlert: Bool = false
+    @State private var showProTips: Bool = true
+    @FocusState private var isTextEditorFocused: Bool
 
     var body: some View {
         ZStack {
@@ -18,61 +20,64 @@ struct TextInputView: View {
                 .fill(Color.green.opacity(0.1))
                 .clipShape(RoundedRectangle(cornerRadius: 18))
             
-            VStack {
-                Text("Curhat dengan ketikan")
-                    .font(.title3)
-                    .fontWeight(.semibold)
-                
-                ZStack {
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(Color.white)
-                        .shadow(radius: 2)
-                        .frame(maxWidth: .infinity, maxHeight: 250)
+                VStack {
+                    Text("Curhat dengan ketikan")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                    
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.white)
+                            .shadow(radius: 2)
+                            .frame(maxWidth: .infinity, maxHeight: 250)
 
-                    TextEditor(text: $textInput)
-                        .padding()
-                        .frame(maxWidth: .infinity, maxHeight: 250)
-                        .cornerRadius(10)
-                }
-                .padding(.bottom, 16)
-                
-                Button {
-                    print("Submit clicked")
-                } label: {
-                    Text("Submit")
-                        .frame(maxWidth: .infinity, maxHeight: 40)
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(8)
-                }
-                
-                Spacer()
-                
-                ProTipsCard()
-                
-                Spacer()
-                
-                Button {
-                    showAlert = true
-                } label: {
-                    HStack {
-                        Image(systemName: "xmark")
-                        Text("Batalkan")
+                        TextEditor(text: $textInput)
+                            .padding()
+                            .frame(maxWidth: .infinity, maxHeight: 250)
+                            .cornerRadius(10)
+                            .focused($isTextEditorFocused)
                     }
-                    .font(.callout)
-                    .fontWeight(.semibold)
-                    .foregroundStyle(.red)
-                    .padding(.horizontal, 12)
-                    .padding(.vertical, 6)
-                    .background(.red.opacity(0.2))
-                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    .padding(.bottom, 16)
+                    
+                    Button {
+                        print("Submit clicked")
+                    } label: {
+                        Text("Submit")
+                            .frame(maxWidth: .infinity, maxHeight: 40)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(8)
+                    }
+                    
+                    Spacer()
+                    
+                    if showProTips {
+                        ProTipsCard()
+                    }
+                    
+                    Spacer()
+                    
+                    Button {
+                        showAlert = true
+                    } label: {
+                        HStack {
+                            Image(systemName: "xmark")
+                            Text("Batalkan")
+                        }
+                        .font(.callout)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.red)
+                        .padding(.horizontal, 12)
+                        .padding(.vertical, 6)
+                        .background(.red.opacity(0.2))
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                    }
                 }
-            }
-            .padding()
+                .padding()
         }
         .safeAreaPadding()
         .navigationBarBackButtonHidden(true)
-        .alert(isPresented :$showAlert) {
+        .alert(isPresented: $showAlert) {
             Alert(
                 title: Text("Batalkan Dokumentasi?"),
                 message: Text("Semua teks yang anda masukkan akan dihapus secara permanen"),
@@ -81,6 +86,14 @@ struct TextInputView: View {
                 },
                 secondaryButton: .cancel(Text("Tidak"))
             )
+        }
+        .onTapGesture {
+            isTextEditorFocused = false
+        }
+        .onChange(of: isTextEditorFocused) { focused in
+            withAnimation {
+                showProTips = !focused
+            }
         }
     }
 }
