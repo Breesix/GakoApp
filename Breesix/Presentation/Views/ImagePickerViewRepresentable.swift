46db32cebb9627940db822276e79ee2c1626f580
akmaalh <akmalhakim2106@gmail.com>
2024-10-19 11:21:46 +0700

    change implementation of image upload

--- Breesix/Presentation/Views/ImagePickerViewRepresentable.swift
+++ Breesix/Presentation/Views/ImagePickerViewRepresentable.swift
@@ -10,6 +10,23 @@ import PhotosUI
 import UIKit
 
 struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    @Environment(\.presentationMode) private var presentationMode
+    var sourceType: UIImagePickerController.SourceType = .photoLibrary
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.sourceType = sourceType
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
     class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
         let parent: ImagePicker
 
@@ -19,30 +36,48 @@ struct ImagePicker: UIViewControllerRepresentable {
 
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
             if let uiImage = info[.originalImage] as? UIImage {
-                parent.selectedImageData = uiImage.jpegData(compressionQuality: 1.0)
+                parent.image = uiImage
             }
-            parent.presentationMode.wrappedValue.dismiss()
-        }
 
-        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
             parent.presentationMode.wrappedValue.dismiss()
         }
     }
-
-    @Environment(\.presentationMode) var presentationMode
-    @Binding var selectedImageData: Data?
-    var sourceType: UIImagePickerController.SourceType
-
-    func makeCoordinator() -> Coordinator {
-        Coordinator(self)
-    }
-
-    func makeUIViewController(context: Context) -> UIImagePickerController {
-        let picker = UIImagePickerController()
-        picker.delegate = context.coordinator
-        picker.sourceType = sourceType
-        return picker
-    }
-
-    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
 }
+
+//struct ImagePicker: UIViewControllerRepresentable {
+//    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+//        let parent: ImagePicker
+//
+//        init(_ parent: ImagePicker) {
+//            self.parent = parent
+//        }
+//
+//        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//            if let uiImage = info[.originalImage] as? UIImage {
+//                parent.selectedImageData = uiImage.jpegData(compressionQuality: 1.0)
+//            }
+//            parent.presentationMode.wrappedValue.dismiss()
+//        }
+//
+//        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+//            parent.presentationMode.wrappedValue.dismiss()
+//        }
+//    }
+//
+//    @Environment(\.presentationMode) var presentationMode
+//    @Binding var selectedImageData: Data?
+//    var sourceType: UIImagePickerController.SourceType
+//
+//    func makeCoordinator() -> Coordinator {
+//        Coordinator(self)
+//    }
+//
+//    func makeUIViewController(context: Context) -> UIImagePickerController {
+//        let picker = UIImagePickerController()
+//        picker.delegate = context.coordinator
+//        picker.sourceType = sourceType
+//        return picker
+//    }
+//
+//    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+//}
