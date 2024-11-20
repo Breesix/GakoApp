c1d5da8a1e0b9af5c54cccf713c17c8bda660a52
Rangga Biner <ranggabiner@gmail.com>
2024-11-20 09:54:56 +0700

    fix: dayeditactivityrow

--- Gako/Presentation/StudentTab/Components/DailyReportTemplate.swift
+++
@@ -1,9 +0,0 @@
-//
-//  DailyReportTemplate.swift
-//  Gako
-//
-//  Created by Kevin Fairuz on 20/11/24.
-//
-import SwiftUI
-
-
--- Gako/Presentation/StudentTab/Components/DayEditActivityRow.swift
+++ Gako/Presentation/StudentTab/Components/DayEditActivityRow.swift
@@ -4,28 +4,31 @@
 //
 //  Created by Kevin Fairuz on 19/11/24.
 //
+//  Copyright © 2024 Gako. All rights reserved.
+//
+//  Description: A custom row component for editing daily activities
+//  Usage: Implement this component in lists or forms where activity editing is needed
+//
+
 import SwiftUI
 
 struct DayEditActivityRow: View {
+    // MARK: - Constants
+    private let spacing: CGFloat = UIConstants.DayEdit.spacing
+    private let activitySectionTitle: String = UIConstants.DayEdit.activitySectionTitle
+    private let titleColor: Color = UIConstants.DayEdit.titleColor
+    
+    // MARK: - Properties
     let activity: Activity
     let index: Int
     @Binding var editedActivities: [UUID: (String, Status, Date)]
     let date: Date
     let onDelete: (Activity) -> Void
     
+    // MARK: - Body
     var body: some View {
-        VStack(alignment: .leading, spacing: UIConstants.DayEdit.spacing) {
-            HStack {
-                Text("\(UIConstants.DayEdit.activitySectionTitle) \(index)")
-                    .font(.callout)
-                    .fontWeight(.bold)
-                    .foregroundColor(UIConstants.DayEdit.titleColor)
-                
-                Spacer()
-                
-                DeleteButton(action: { onDelete(activity) })
-            }
-            
+        VStack(alignment: .leading, spacing: spacing) {
+            activitySection
             EditTextField(
                 text: EditBindingHelper.makeActivityBinding(
                     activity: activity,
@@ -33,7 +36,6 @@ struct DayEditActivityRow: View {
                     date: date
                 )
             )
-            
             StatusPicker(
                 status: EditBindingHelper.makeStatusBinding(
                     activity: activity,
@@ -46,4 +48,31 @@ struct DayEditActivityRow: View {
             }
         }
     }
+    
+    // MARK: - Subview
+    private var activitySection: some View {
+        HStack {
+            Text("\(activitySectionTitle) \(index)")
+                .font(.callout)
+                .fontWeight(.bold)
+                .foregroundColor(titleColor)
+            Spacer()
+            DeleteButton(action: { onDelete(activity) })
+        }
+    }
+}
+
+// MARK: - Preview
+#Preview {
+    DayEditActivityRow(
+        activity: Activity(
+            id: UUID(),
+            activity: "Sample Activity",
+            status: .dibimbing, student: .init(fullname: "Rangga", nickname: "rangga")
+        ),
+        index: 1,
+        editedActivities: .constant([:]),
+        date: Date(),
+        onDelete: { _ in }
+    )
 }
